f39bd2a8c8c2b17119566f80b0934c26d04ea08d
Bob Rudis <bob@rud.is>
2017-09-26 11:33:00 -0400

    faster pkg data bootstrapping

--- R/zzz.r
+++ R/zzz.r
@@ -1,10 +1,18 @@
 .pkgenv <- new.env(parent=emptyenv())
 
-.onAttach <- function(...) {
+.onLoad <- function(...) {
 
-  if (interactive()) packageStartupMessage("Building application inventory...")
+  # if (interactive()) packageStartupMessage("Building application inventory...")
+  #
+  # rebuild_apps_inventory(system.file("extdata", "apps.json", package = "rappalyzer"))
 
-  rebuild_apps_inventory(system.file("extdata", "apps.json", package = "rappalyzer"))
+  assign("rapp_join_df", rapp_join_df, envir=.pkgenv)
+  # assign("apps", apps, envir=.pkgenv)
+  assign("html", html, envir=.pkgenv)
+  assign("script", script, envir=.pkgenv)
+  assign("urls", urls, envir=.pkgenv)
+  assign("meta", meta, envir=.pkgenv)
+  assign("headers", headers, envir=.pkgenv)
 
 }
 
