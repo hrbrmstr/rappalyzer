59c40d29229b5560dc441ab61ad81b6a7a22aceb
Bob Rudis <bob@rud.is>
2017-09-26 11:39:29 -0400

    non-serialize-able external pointers == reverting speedup code

--- R/zzz.r
+++ R/zzz.r
@@ -2,17 +2,9 @@
 
 .onLoad <- function(...) {
 
-  # if (interactive()) packageStartupMessage("Building application inventory...")
-  #
-  # rebuild_apps_inventory(system.file("extdata", "apps.json", package = "rappalyzer"))
+  if (interactive()) packageStartupMessage("Building application inventory...")
 
-  assign("rapp_join_df", rapp_join_df, envir=.pkgenv)
-  # assign("apps", apps, envir=.pkgenv)
-  assign("html", html, envir=.pkgenv)
-  assign("script", script, envir=.pkgenv)
-  assign("urls", urls, envir=.pkgenv)
-  assign("meta", meta, envir=.pkgenv)
-  assign("headers", headers, envir=.pkgenv)
+  rebuild_apps_inventory(system.file("extdata", "apps.json", package = "rappalyzer"))
 
 }
 
